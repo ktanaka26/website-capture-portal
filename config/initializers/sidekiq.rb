809ec593420a39a328ad65c50a9d0ba6442acc92
Kohei Tanaka <github@ktanaka26.net>
2025-03-30 20:32:37 +0900

    RedisへTLSで接続するようにした

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,35 @@
+# frozen_string_literal: true
+
 Sidekiq.configure_server do |config|
-  config.redis = { url: Rails.application.credentials[:redis][:url] }
+  config.redis = {
+    url: ENV['REDIS_URL'] || Rails.application.credentials[:redis][:url],
+    username: ENV['REDIS_USERNAME'] || Rails.application.credentials[:redis][:username],
+    password: ENV['REDIS_PASSWORD'] || Rails.application.credentials[:redis][:password],
+    ssl: true,
+    ssl_params: {
+      cert: './certs/client-cert.pem',
+      key: './certs/client-key.pem',
+      ca_file: './certs/ca-cert.pem',
+      verify_mode: OpenSSL::SSL::VERIFY_NONE,
+      min_version: OpenSSL::SSL::TLS1_2_VERSION,
+
+    }
+  }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: Rails.application.credentials[:redis][:url] }
+  config.redis = {
+    url: ENV['REDIS_URL'] || Rails.application.credentials[:redis][:url],
+    username: ENV['REDIS_USERNAME'] || Rails.application.credentials[:redis][:username],
+    password: ENV['REDIS_PASSWORD'] || Rails.application.credentials[:redis][:password],
+    ssl: true,
+    min_version: 'TLSv1',
+    ssl_params: {
+      cert: './certs/client-cert.pem',
+      key: './certs/client-key.pem',
+      ca_file: './certs/ca-cert.pem',
+      verify_mode: OpenSSL::SSL::VERIFY_NONE,
+      min_version: OpenSSL::SSL::TLS1_2_VERSION,
+    }
+  }
 end
